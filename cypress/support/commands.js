325501d82cf539ab1fd43e57e87794dc16409cef
Sharu1508 <sharumathikumaresan@gmail.com>
2024-07-26 16:39:25 +0200

    Dependencies added to generate mochawesome reports-Headed and Headless

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,6 +25,13 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 
-///<reference types= "cypress"/>
+///<reference types= "Cypress"/>
 
 ///<reference types= "cypress-xpath"/>
+
+Cypress.Commands.add('getIframe',(iframe)=>{
+     return cy.get(iframe)
+    .its('0.contentDocument.body')
+    .should('be.visible')
+    .then(cy.wrap);
+})
